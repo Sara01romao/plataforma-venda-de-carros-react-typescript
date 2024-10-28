136984e3cd30802eb83830832075afc5d5caa8cf
Sara Romao <sara-romao@live.com>
2024-10-28 08:28:08 -0300

    feat: load imagens position

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,76 @@
-import { Container } from "../../components/container"
+import { useEffect, useState } from "react";
+import { Container } from "../../components/container";
+
+import {
+    collection,
+    query, 
+    getDocs,
+    orderBy
+} from 'firebase/firestore';
+import { db } from "../../services/firebaseConnection";
+import { string } from "zod";
+import { Link } from "react-router-dom";
+
+interface CarsProps{
+    id: string;
+    name: string;
+    year: string;
+    uid: string;
+    price: string | number;
+    city: string;
+    km: string;
+    images: CarImageProps[];
+    url: string;
+}
+
+interface CarImageProps{
+    name: string;
+    uid: string;
+    url: string;
+
+}
 
 export function Home(){
+
+    const [cars, setCars] = useState<CarsProps[]>([]);
+    const [loadImages, setLoadImages] = useState<string[]>([])
+
+    useEffect(() =>{
+        function loadCars(){
+            const carsRef = collection(db, "cars");
+            const queryRef = query(carsRef, orderBy('created', 'desc'));
+
+            getDocs(queryRef)
+            .then((snapshot)=>{
+               let listcars = [] as CarsProps[];
+
+               snapshot.forEach(doc =>{
+                listcars.push({
+                    id: doc.id,
+                    name: doc.data().name,
+                    year: doc.data().year,
+                    km: doc.data().km,
+                    city: doc.data().city,
+                    price: doc.data().price,
+                    images: doc.data().images,
+                    uid: doc.data().uid,
+                    url: doc.data().url
+                   
+                    
+                })
+               })
+
+               setCars(listcars);
+            })
+        }
+
+        loadCars()
+    },[])
+
+    function handleImageLoad(id:string){
+        setLoadImages((prevImageLoaded) => [...prevImageLoaded, id]);
+
+    }
 
     return(
         <Container>
@@ -22,27 +92,38 @@ export function Home(){
             </h1>
 
             <main className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                <section className="w-full bg-white rounded-lg">
-                    <img 
-                        className="w-full rounded-lg mb-2 mx-h-72 hover:scale-105 transition-all"
-                        src="" 
-                        alt="Carro" 
-                    />
-                    <p className="font-bold mt-l mb-2 px-2" >BMW</p>
+                {cars.map(car => (
+                    <Link key={car.id} to={`/car/${car.id}`} >
+                        <section className="w-full bg-white rounded-lg">
+                            <div 
+                                className="w-full h-72 rounded-lg bg-slate-200"
+                                style={{display: loadImages.includes(car.id) ? "none" : "block"}}
+                            ></div>
+                            <img 
+                                className="w-full rounded-lg mb-2 mx-h-72 hover:scale-105 transition-all"
+                                src={car.images[0].url}
+                                alt="Carro" 
+                                onLoad={() => handleImageLoad(car.id)}
+                                style={{display: loadImages.includes(car.id) ? "block" : "none"}}
+                            />
+                            <p className="font-bold mt-l mb-2 px-2" >{car.name}</p>
 
-                    <div className="flex flex-col px-2">
-                        <span className="text-zinc-700 mb-6">Ano 216/2016 | 23.000 km</span>
-                        <strong className="text-black font-medium text-xl">R$ 190.000</strong>
-                    </div>
+                            <div className="flex flex-col px-2">
+                                <span className="text-zinc-700 mb-6">Ano {car.year} | {car.km} km</span>
+                                <strong className="text-black font-medium text-xl">R$ {car.price}</strong>
+                            </div>
 
-                    <div></div>
+                            <div></div>
 
-                    <div>
-                        <span>
-                            Campo Grande -MS
-                        </span>
-                    </div>
-                </section>
+                            <div className="p-2">
+                                <span>
+                                    {car.city}
+                                </span>
+                            </div>
+                        </section>
+                    </Link>
+                ))}
+                
             </main>
        
         </Container>
